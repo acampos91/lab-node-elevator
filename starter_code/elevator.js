a511abcd4436a08a87badf55e3d3c1f80841b4b4
acampos91 <acampos91@hotmail.com>
2017-11-06 20:17:47 +0100

    elevator exercise

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,63 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.waitingList = [];
+    this.passengers = [];
+    this.direction = "up";
+    this.setInterval;
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+  this.setInterval = setInterval(() => {
+    this.update();
+  }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.setInterval);
+  }
+  update() {
+this.log();
+  }
+  _passengersEnter(person) {
+if(!this.waitingList.length == 0){
+  this.passengers.push(person);
+  this.waitingList.pop();
+  requests.destinationFloor.push(person.originFloor);
+  console.log(person.name + "has enter the elevator");
+}
+  }
+  _passengersLeave(person) {
+    if(!this.passengers == 0){
+      if (person.destinationFloor == this.floor){
+        this.passengers.pop(person)
+      }
+      console.log(person.name + "has left the elevator");
+    }
+
+   }
+
+  floorUp() {
+  if(this.floor < this.MAXFLOOR){
+  this.floor ++;
+  console.log(this.log());
+   }
+  }
+  floorDown() {
+  if(this.floor > 0){
+  this.floor --;
+  console.log(this.log());
+} else {
+  this.floor = 0;
+}
+   }
+  call(person) {
+this.waitingList.push(person);
+this.requests.push(person.originFloor)
+   }
+  log() {
+    console.log("Direction: " + this.direction + " Floor: " + this.floor)
+  }
 }
 
 module.exports = Elevator;
